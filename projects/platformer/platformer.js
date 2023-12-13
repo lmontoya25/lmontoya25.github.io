c47ed35e19e218c70f4eb95c3edd0470355ce675
lmontoya25 montoya <Lmontoya25@livingschoolnola.org>
2023-12-13 17:37:37 +0000

    push

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,27 +43,34 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+createPlatform(300,700,200,20)
+createPlatform(500,600,200,10)
+createPlatform(700,500,200,15)
+createPlatform(900,400,200,10)
+createPlatform(1000,300,200,10)
 
 
 
-    
+
+
+
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
+createCollectable('database',300,500,1,1)
+createCollectable('database',500,100,1,1)
+createCollectable('database',700,300,10,10)
 
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
+createCannon('left',400,1000,10,10)
+createCannon('left',300,2000,10,10)
+createCannon('left',200,3000,10,10)
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
